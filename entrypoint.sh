eb6b9fb3be87657da7acfba94144c1fe19a1b3b4
Kaushik Devireddy <kdev@ucla.edu>
2021-06-12 22:40:57 +0000

    Post comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,22 @@
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
+
+post_to_github () {
+  if [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then
+    GITHUB_SHA=$(cat $GITHUB_EVENT_PATH | jq -r .pull_request.head.sha)
+  fi
+
+  if [ -z "$GITHUB_TOKEN" ]; then
+    echo "Error: GITHUB_TOKEN is required to post comment to GitHub"
+  else
+    echo "Posting comment to GitHub commit $GITHUB_SHA"
+    msg="$(build_msg true)"
+    jq -Mnc --arg msg "$msg" '{"body": "Asdf"}' | curl -L -X POST -d @- \
+      -H "Content-Type: application/json" \
+      -H "Authorization: token $GITHUB_TOKEN" \
+      "https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA/comments"
+  fi
+}
+
+post_to_github
